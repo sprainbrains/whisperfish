919ec442fa2ce876eac24f2d4d2df8032ef30541
Ruben De Smet <ruben.de.smet@rubdos.be>
2019-07-24 10:28:26 +0200

    Add some debug statements

--- .ci/build.sh
+++ .ci/build.sh
@@ -6,7 +6,16 @@ echo Building for SailfishOS-$SFOS_VERSION-$TARGET
 sb2 -t SailfishOS-$SFOS_VERSION-$TARGET \
     -m sdk-install \
     -R zypper --non-interactive in cmake git make
+echo Copying source
 sudo cp -r . ~nemo/src
 sudo chown -R nemo:nemo ~nemo/src
 cd ~nemo/src
+
+echo Done copying source.
+pwd
+
 mb2 -t SailfishOS-$SFOS_VERSION-$TARGET build
+
+echo Done building
+pwd
+ls RPMS
