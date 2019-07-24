50f878322c52fe342a93243d0fbfbb66550c5470
Ruben De Smet <ruben.de.smet@rubdos.be>
2019-07-24 15:17:12 +0200

    Denote stubs.

--- src/model/session.cpp
+++ src/model/session.cpp
@@ -1,13 +1,16 @@
 #include "model/session.hpp"
 
 int SessionModel::rowCount(const QModelIndex &parent) const {
+    // TODO
     return 0;
 }
 
 QVariant SessionModel::data(const QModelIndex &index, int role) const {
+    // TODO
     return QVariant::fromValue(true);
 }
 
 int SessionModel::unread() const {
+    // TODO
     return 0;
 }
