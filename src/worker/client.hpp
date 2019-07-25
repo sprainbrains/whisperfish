7c3d3a5ab6ef2616c52c2d04aceb6c351aee9661
Ruben De Smet <ruben.de.smet@rubdos.be>
2019-07-25 15:30:49 +0200

    Connect to the WebSocket with the correct root CA.

--- src/worker/client.hpp
+++ src/worker/client.hpp
@@ -1,17 +1,27 @@
 #pragma once
 
 #include <QtCore>
+#include <QtWebSockets/QtWebSockets>
+#include <QtNetwork>
 
 class ClientWorker : public QObject {
     Q_OBJECT
 
 public:
-    ClientWorker(QObject *parent = nullptr): QObject(parent) {
-    }
+    ClientWorker(QObject *parent = nullptr);
 
 signals:
     void messageReceived(int sid, int mid);
     void messageReceipt(int sid, int mid);
     void notifyMessage(int sid, QString source, QString message, bool isGroup);
     void promptResetPeerIdentity(QString source);
+
+private slots:
+    void onConnected();
+    void onTextMessageReceived(const QString message);
+    void onSslErrors(const QList<QSslError>);
+    void onError(QAbstractSocket::SocketError);
+
+private:
+    QWebSocket wss;
 };
