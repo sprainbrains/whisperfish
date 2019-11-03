d5afcd315a486db0fcb754b676d88b92c08a55d1
Ruben De Smet <ruben.de.smet@rubdos.be>
2019-11-03 21:27:38 +0100

    Rust fixup

--- .ci/build.sh
+++ .ci/build.sh
@@ -17,26 +17,18 @@ echo Done copying source.
 pwd
 
 # Rust
-case "$TARGET" in
-    i486 )
-        export RUST_TARGET=i586-unknown-linux-gnu ;;
-    armv7hl )
-        export RUST_TARGET=arm-unknown-linux-gnueabihf ;;
-esac
-
-echo Building for Rust target $RUST_TARGET
-
 curl --proto '=https' --tlsv1.2 -sSf -o ~nemo/rustup.sh https://sh.rustup.rs
 sb2 -t SailfishOS-$SFOS_VERSION-$TARGET -m sdk-install \
     sh ~nemo/rustup.sh \
         --profile minimal \
-        --target $RUST_TARGET \
         -y \
+
+source $HOME/.cargo/env
 
 (
     cd ~nemo/src
     sb2 -t SailfishOS-$SFOS_VERSION-$TARGET -m sdk-build \
-        cargo build --target=$RUST_TARGET
+        cargo build
     echo Done building Rust version
 )
 
