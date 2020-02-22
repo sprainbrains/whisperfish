c27ccaca4cc0d176805c5a824f18511bbbc7f073
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-02-22 23:58:39 +0100

    qmetaobject-sfos, build against platform sdk

--- build.rs
+++ build.rs
@@ -12,21 +12,22 @@ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FO
 OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-use std::process::Command;
 use std::env;
 use std::path::Path;
 use std::io::Write;
 
 use failure::*;
 
-fn qmake_query(var: &str) -> String {
-    String::from_utf8(
-        Command::new("qmake")
-            .args(&["-query", var])
-            .output()
-            .expect("Failed to execute qmake. Make sure 'qmake' is in your path")
-            .stdout,
-    ).expect("UTF-8 conversion failed")
+fn mock_libc(mer_root: &str) -> Result<String, Error> {
+    let out_dir = env::var("OUT_DIR")?;
+    let qml_path = &Path::new(&out_dir).join("libc.so");
+
+    let mut f = std::fs::File::create(qml_path)?;
+    writeln!(f, "OUTPUT_FORMAT(elf32-littlearm)")?;
+    writeln!(f, "GROUP ( {}/lib/libc.so.6 {}/usr/lib/libc_nonshared.a  AS_NEEDED ( {}/lib/ld-linux-armhf.so.3 ))",
+        mer_root, mer_root, mer_root)?;
+
+    Ok(out_dir)
 }
 
 fn qml_to_qrc() -> Result<(), Error> {
@@ -38,7 +39,7 @@ fn qml_to_qrc() -> Result<(), Error> {
     let mut read_dirs = std::collections::VecDeque::new();
     read_dirs.push_back(std::fs::read_dir("qml")?);
 
-    write!(f, "qrc!{{qml_resources, \"/\" {{ ")?;
+    writeln!(f, "qrc!{{qml_resources, \"qml\" {{ ")?;
 
     while let Some(read_dir) = read_dirs.pop_front() {
         for entry in read_dir {
@@ -47,22 +48,35 @@ fn qml_to_qrc() -> Result<(), Error> {
             if entry.is_dir() {
                 read_dirs.push_back(std::fs::read_dir(entry)?);
             } else if entry.is_file() {
-                write!(f, "{:?},", entry)?;
+                writeln!(f, "{:?},", entry)?;
             }
         }
     }
 
-    write!(f, " }} }}")?;
+    writeln!(f, " }} }}")?;
 
     Ok(())
 }
 
 fn main() {
-    let qt_include_path = qmake_query("QT_INSTALL_HEADERS");
-    let qt_library_path = qmake_query("QT_INSTALL_LIBS");
+    let mer_sdk = std::env::var("MERSDK").ok()
+        .expect("MERSDK should be set.");
+
+    let mer_target = std::env::var("MER_TARGET").ok().unwrap_or("SailfishOS-latest".into());
+
+    let arch = match &std::env::var("CARGO_CFG_TARGET_ARCH").unwrap() as &str {
+        "arm" => "armv7hl",
+        "i686" => "i486",
+        unsupported => panic!("Target {} is not supported for Mer", unsupported),
+    };
+
+    let mer_target_root = format!("{}/targets/{}-{}", mer_sdk, mer_target, arch);
 
     cpp_build::Config::new()
-        .include(qt_include_path.trim())
+        .include(format!("{}/usr/include/", mer_target_root))
+        .include(format!("{}/usr/include/sailfishapp/", mer_target_root))
+        .include(format!("{}/usr/include/qt5/", mer_target_root))
+        .include(format!("{}/usr/include/qt5/QtCore", mer_target_root))
         .build("src/main.rs");
 
     let macos_lib_search = if cfg!(target_os = "macos") {
@@ -72,17 +86,49 @@ fn main() {
     };
     let macos_lib_framework = if cfg!(target_os = "macos") { "" } else { "5" };
 
+    let mock_lib_path = mock_libc(&mer_target_root).unwrap();
     println!(
         "cargo:rustc-link-search{}={}",
         macos_lib_search,
-        qt_library_path.trim()
+        mock_lib_path,
     );
-    println!("cargo:rustc-link-lib{}=Qt{}Widgets", macos_lib_search, macos_lib_framework);
-    println!("cargo:rustc-link-lib{}=Qt{}Gui", macos_lib_search, macos_lib_framework);
-    println!("cargo:rustc-link-lib{}=Qt{}Core", macos_lib_search, macos_lib_framework);
-    println!("cargo:rustc-link-lib{}=Qt{}Quick", macos_lib_search, macos_lib_framework);
-    println!("cargo:rustc-link-lib{}=Qt{}Qml", macos_lib_search, macos_lib_framework);
-    println!("cargo:rustc-link-lib{}=Qt{}QuickControls2", macos_lib_search, macos_lib_framework);
+
+    println!("cargo:rustc-bin-link-arg=-rpath-link,{}/usr/lib", mer_target_root);
+    println!("cargo:rustc-bin-link-arg=-rpath-link,{}/lib", mer_target_root);
+
+    println!(
+        "cargo:rustc-link-search{}={}/toolings/{}/opt/cross/{}-meego-linux-gnueabi/lib",
+        macos_lib_search,
+        mer_sdk, mer_target, arch
+    );
+
+    println!(
+        "cargo:rustc-link-search{}={}/usr/lib/qt5/qml/Nemo/Notifications/",
+        macos_lib_search,
+        mer_target_root
+    );
+
+    println!(
+        "cargo:rustc-link-search{}={}/toolings/{}/opt/cross/lib/gcc/{}-meego-linux-gnueabi/4.9.4/",
+        macos_lib_search,
+        mer_sdk, mer_target, arch
+    );
+
+    println!(
+        "cargo:rustc-link-search{}={}/usr/lib/",
+        macos_lib_search,
+        mer_target_root,
+    );
+
+    let qt_libs = ["OpenGL", "Gui", "Core", "Quick", "Qml"];
+    for lib in &qt_libs {
+        println!("cargo:rustc-link-lib{}=Qt{}{}", macos_lib_search, macos_lib_framework, lib);
+    }
+
+    let libs = ["EGL", "nemonotifications", "sailfishapp"];
+    for lib in &libs {
+        println!("cargo:rustc-link-lib{}={}", macos_lib_search, lib);
+    }
 
     qml_to_qrc().unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![recursion_limit="256"]
+
 #[macro_use]
 extern crate cpp;
 
@@ -6,18 +8,48 @@ use qmetaobject::*;
 mod qrc;
 
 fn main() {
+    qrc::load();
+
     unsafe {
         cpp! { {
             #include <QtCore/QCoreApplication>
             #include <QtCore/QString>
+
+            #include <QtGui/QGuiApplication>
+            #include <QtQuick/QQuickView>
+            #include <QtQml/QtQml>
+            #include <QtCore/QtCore>
+
+            #include <sailfishapp.h>
         }}
         cpp!{[]{
-            QCoreApplication::setApplicationName(QStringLiteral("harbour-whisperfish"));
+            int argc = 1;
+            char *_appName = "harbour-whisperfish";
+            char **argv = &_appName;
+            QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
+            qApp->setApplicationVersion("0.6.0");
+
+            QTranslator translator(qApp);
+            const QString appName = qApp->applicationName();
+            const QString transDir = SailfishApp::pathTo(QStringLiteral("translations")).toLocalFile();
+            const QLocale locale;
+            if (!translator.load(locale, appName, "-", transDir, ".qm")) {
+                qWarning() << "Failed to load translator for" << QLocale::system().uiLanguages()
+                           << "Searched" << transDir << "for" << appName;
+                if(!translator.load(appName, transDir)) {
+                    qWarning() << "Could not load default translator either!";
+                }
+                app->installTranslator(&translator);
+            }
+
+            QScopedPointer<QQuickView> view(SailfishApp::createView());
+
+            view->setSource(SailfishApp::pathTo("qml/harbour-whisperfish.qml"));
+            app->exec();
         }}
     }
-    qrc::load();
 
-    let mut engine = QmlEngine::new();
-    engine.load_file("qrc:/qml/harbour-whisperfish.qml".into());
-    engine.exec();
+    // let mut engine = QmlEngine::new();
+    // engine.load_file("qrc:/qml/harbour-whisperfish.qml".into());
+    // engine.exec();
 }
