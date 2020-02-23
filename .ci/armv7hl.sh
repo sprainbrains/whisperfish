439e9567ab4580876ca185e352b85a1433e6090a
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-02-23 10:36:52 +0100

    Fix CC/AR paths in CI

--- .ci/armv7hl.sh
+++ .ci/armv7hl.sh
@@ -1,3 +1,3 @@
 export RUSTUP_TARGET=armv7-unknown-linux-gnueabihf
-export RUSTUP_CC=$MERSDK/toolings/$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-gcc
-export RUSTUP_AR=$MERSDK/toolings/$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-ar
+export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-gcc
+export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-ar
--- .ci/i486.sh
+++ .ci/i486.sh
@@ -1,3 +1,3 @@
 export RUSTUP_TARGET=i686-unknown-linux-gnu
-export RUSTUP_CC=$MERSDK/toolings/$SFOS_VERSION/opt/cross/bin/i486-meego-linux-gnueabi-gcc
-export RUSTUP_AR=$MERSDK/toolings/$SFOS_VERSION/opt/cross/bin/i486-meego-linux-gnueabi-ar
+export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/i486-meego-linux-gnueabi-gcc
+export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/i486-meego-linux-gnueabi-ar
