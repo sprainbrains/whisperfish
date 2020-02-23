a4b655b4f00e0fccc95a79f491c98cf7096ba7a6
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-02-23 10:21:53 +0100

    Try to fix CI

--- .ci/build.sh
+++ .ci/build.sh
@@ -3,32 +3,28 @@
 set -e
 
 echo Building for SailfishOS-$SFOS_VERSION-$TARGET
-sb2 -t SailfishOS-$SFOS_VERSION-$TARGET \
-    -m sdk-install \
-    -R zypper --non-interactive in cmake git make openssl-devel \
-        qt5-qtwebsockets qt5-qtwebsockets-devel \
 
-
-echo Copying source
-sudo cp -r . ~nemo/src
-sudo chown -R nemo:nemo ~nemo/src
-
-echo Done copying source.
-pwd
+export MERSDK=/srv/mer
+export MER_TARGET="SailfishOS-$SFOS_VERSION"
+export RUSTFLAGS="-C link-args=-Wl,-rpath-link,$MERSDK/targets/$MER_TARGET-$TARGET/usr/lib/,-rpath-link,$MERSDK/targets/$MER_TARGET-$TARGET/lib/"
 
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf -o ~nemo/rustup.sh https://sh.rustup.rs
-sb2 -t SailfishOS-$SFOS_VERSION-$TARGET -m sdk-install \
-    sh ~nemo/rustup.sh \
-        --profile minimal \
-        -y \
+sh ~nemo/rustup.sh \
+    --profile minimal \
+    -y \
 
 source $HOME/.cargo/env
 
-(
-    cd ~nemo/src
-    mb2 -t SailfishOS-$SFOS_VERSION-$TARGET build
-    echo Done building Rust version
-)
+source ./$TARGET.sh
+rustup target add $RUSTUP_TARGET
+
+cat <<EOF > ~/.cargo/config
+[target.$RUSTUP_TARGET]
+linker = "$RUSTUP_CC"
+ar = "$RUSTUP_AR"
+EOF
+
+cargo build
 
 sudo cp -r /home/nemo/src/RPMS RPMS
