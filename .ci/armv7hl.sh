5dffaa145ffd880478bc6fd4798c9b246c895ce8
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-02-23 19:38:00 +0100

    CI: Set compiler paths

--- .ci/armv7hl.sh
+++ .ci/armv7hl.sh
@@ -1,3 +1,5 @@
 export RUSTUP_TARGET=armv7-unknown-linux-gnueabihf
 export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-gcc
 export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-ar
+
+export CC_armv7_unknown_linux_gnueabihf="$RUSTUP_CC"
--- .ci/i486.sh
+++ .ci/i486.sh
@@ -1,3 +1,5 @@
 export RUSTUP_TARGET=i686-unknown-linux-gnu
 export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/i486-meego-linux-gnueabi-gcc
 export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/i486-meego-linux-gnueabi-ar
+
+export CC_i686_unknown_linux_gnu="$RUSTUP_CC"
