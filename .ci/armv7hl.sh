160badef427c7f0f19433f5f2a07f33637d2678f
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-02-25 15:27:23 +0100

    Fixup rust compiler

--- .ci/armv7hl.sh
+++ .ci/armv7hl.sh
@@ -1,4 +1,4 @@
-export RUSTUP_TARGET=armv7-unknown-linux-gnueabihf
+export RUSTUP_TARGET=arm-unknown-linux-gnueabihf
 export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-gcc
 export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-ar
 
