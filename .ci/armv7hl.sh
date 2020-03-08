104a091e9a147a867e391e6b2c5f7ff4d53374af
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-03-08 16:02:13 +0100

    armv7-unknown-linux-gnueabi

--- .ci/armv7hl.sh
+++ .ci/armv7hl.sh
@@ -1,8 +1,8 @@
-export RUSTUP_TARGET=arm-unknown-linux-gnueabihf
+export RUSTUP_TARGET=armv7-unknown-linux-gnueabi
 export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-gcc
 export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-ar
 
-export CC_arm_unknown_linux_gnueabihf="$RUSTUP_CC"
+export CC_armv7_unknown_linux_gnueabi="$RUSTUP_CC"
 
 # XXX: potentially also add +neon
 export RUSTFLAGS="$RUSTFLAGS -C target-feature=+v7"
