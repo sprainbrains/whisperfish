b9ff847109cc5c313d9a97b1b32e5e9835829bf7
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-03-08 15:25:55 +0100

    ARM: +v7

--- .ci/armv7hl.sh
+++ .ci/armv7hl.sh
@@ -3,3 +3,6 @@ export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl
 export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-ar
 
 export CC_arm_unknown_linux_gnueabihf="$RUSTUP_CC"
+
+# XXX: potentially also add +neon
+export RUSTFLAGS="$RUSTFLAGS -C target-feature=+v7"
