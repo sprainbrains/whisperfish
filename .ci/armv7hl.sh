1e0327ab170cdb01a8d35d6a1e17fb2089a85be2
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-03-08 10:42:43 +0100

    Fixup CC for armv7

--- .ci/armv7hl.sh
+++ .ci/armv7hl.sh
@@ -2,4 +2,4 @@ export RUSTUP_TARGET=arm-unknown-linux-gnueabihf
 export RUSTUP_CC=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-gcc
 export RUSTUP_AR=$MERSDK/toolings/SailfishOS-$SFOS_VERSION/opt/cross/bin/armv7hl-meego-linux-gnueabi-ar
 
-export CC_armv7_unknown_linux_gnueabihf="$RUSTUP_CC"
+export CC_arm_unknown_linux_gnueabihf="$RUSTUP_CC"
