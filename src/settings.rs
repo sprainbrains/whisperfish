38763ab5c22ad8b943cfdbe442d87b4b261b8224
Ruben De Smet <ruben.de.smet@rubdos.be>
2020-11-08 16:44:02 +0100

    Fixup modifying settings

--- src/settings.rs
+++ src/settings.rs
@@ -37,25 +37,23 @@ cpp! {{
     #include <QtCore/QSettings>
 }}
 
-cpp_class!(
-    unsafe struct QSettings as "QSettings"
-);
-
-impl QSettings {
+impl Settings {
     fn value_bool(&self, key: &str) -> bool {
         let key = QString::from(key);
         unsafe {
-            cpp!([self as "QSettings *", key as "QString"] -> bool as "bool" {
-                return self->value(key).toBool();
+            cpp!([key as "QString"] -> bool as "bool" {
+                QSettings settings;
+                return settings.value(key).toBool();
             })
         }
     }
 
-    fn set_bool(&self, key: &str, value: bool) {
+    fn set_bool(&mut self, key: &str, value: bool) {
         let key = QString::from(key);
         unsafe {
-            cpp!([self as "QSettings *", key as "QString", value as "bool"] {
-                self->setValue(key, value);
+            cpp!([key as "QString", value as "bool"] {
+                QSettings settings;
+                settings.setValue(key, value);
             })
         };
     }
@@ -63,19 +61,21 @@ impl QSettings {
     fn value_string(&self, key: &str) -> String {
         let key = QString::from(key);
         let val = unsafe {
-            cpp!([self as "QSettings *", key as "QString"] -> QString as "QString" {
-                return self->value(key).toString();
+            cpp!([key as "QString"] -> QString as "QString" {
+                QSettings settings;
+                return settings.value(key).toString();
             })
         };
         val.into()
     }
 
-    fn set_string(&self, key: &str, value: &str) {
+    fn set_string(&mut self, key: &str, value: &str) {
         let key = QString::from(key);
         let value = QString::from(value);
         unsafe {
-            cpp!([self as "QSettings *", key as "QString", value as "QString"] {
-                self->setValue(key, value);
+            cpp!([ key as "QString", value as "QString"] {
+                QSettings settings;
+                settings.setValue(key, value);
             })
         };
     }
@@ -96,15 +96,13 @@ pub struct Settings {
     boolValue: qt_method!(fn(&self, key: String) -> bool),
 
     defaults: qt_method!(fn(&self)),
-
-    inner: QSettings,
 }
 
 impl Settings {
     #[allow(non_snake_case)]
     fn stringSet(&mut self, key: String, val: String) {
         log::info!("Setting string {}", key);
-        self.inner.set_string(&key, &val)
+        self.set_string(&key, &val)
     }
 
     #[allow(non_snake_case)]
@@ -115,7 +113,7 @@ impl Settings {
     #[allow(non_snake_case)]
     fn boolSet(&mut self, key: String, val: bool) {
         log::info!("Setting bool {}", key);
-        self.inner.set_bool(&key, val)
+        self.set_bool(&key, val)
     }
 
     #[allow(non_snake_case)]
@@ -123,16 +121,24 @@ impl Settings {
         self.get_bool(key)
     }
 
-    fn defaults(&mut self) {
-        // FIXME
-        log::error!("Setting default settings unimplemented.");
+    pub fn defaults(&mut self) {
+        log::info!("Setting default settings.");
+
+        self.set_bool("incognito", false);
+        self.set_bool("enable_notify", true);
+        self.set_bool("show_notify_message", false);
+        self.set_bool("encrypt_database", true);
+        self.set_bool("save_attachments", true);
+        self.set_bool("share_contacts", true);
+        self.set_bool("enable_enter_send", false);
+        self.set_bool("scale_image_attachments", false);
     }
 
     pub fn get_string(&self, key: impl AsRef<str>) -> String {
-        self.inner.value_string(key.as_ref())
+        self.value_string(key.as_ref())
     }
 
     pub fn get_bool(&self, key: impl AsRef<str>) -> bool {
-        self.inner.value_bool(key.as_ref())
+        self.value_bool(key.as_ref())
     }
 }
