0dfc43ed2f9333f860ee2952d414b1210c1a7beb
Oğuz Ersen <oguzersen@protonmail.com>
2021-01-28 14:40:07 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (143 of 143 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/tr/

--- translations/harbour-whisperfish-tr.ts
+++ translations/harbour-whisperfish-tr.ts
@@ -207,7 +207,7 @@
         <location filename="../qml/pages/LinkedDevices.qml" line="65"/>
         <source>Current device (Whisperfish, %1)</source>
         <extracomment>Linked device title for current Whisperfish</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Geçerli aygıt (Whisperfish, %1)</translation>
     </message>
     <message id="whisperfish-device-name">
         <location filename="../qml/pages/LinkedDevices.qml" line="69"/>
