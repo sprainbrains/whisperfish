c636fcbd6ce94243d7208975fa5b8eeaf5cf0fe3
Mirian Margiani <mixosaurus+ichthyo@pm.me>
2021-02-02 12:38:02 +0100

    Update live-patch.sh to run as user and watch for fs events
    It required root access before, now it requires 'watchit'...

--- live-patch.sh
+++ live-patch.sh
@@ -1,55 +1,131 @@
 #!/bin/bash
+#
+# Requires watchit (https://github.com/ichthyosaurus/watchit) and pyinotify
+# (https://github.com/seb-m/pyinotify).
+#
+# Environment:
+# SSH_TARGET=nemo@phone     how to connect to the target device
+# NO_BUILD=1                if building is disabled (building is enabled by default)
+#                           This can be specified as environment variable or as
+#                           command line argument. If building is disabled, the
+#                           executable is expected in build/harbour-whisperfish.
+#                           Manually extract it from the latest RPM.
+#
+# Configuration:
+# Change $cSHADOW to a user-writable path on your device. The default is
+# /opt/sdk/harbour-whisperfish.
+#
+# The script will run from Whisperfish's root directory. It watches for changed
+# (modified, added, removed) files, then rebuilds and deploys them as needed.
+
 set -eu
 cd "$(dirname -- "$(type greadlink >/dev/null 2>&1 && greadlink -f -- "$0" || readlink -f -- "$0")")"
 
-# Source .env for SSH_TARGET
-[ -e ".env" ] && source ./.env
+if ! type watchit >/dev/null 2>&1; then
+    echo "error: this script requires 'watchit' (https://github.com/ichthyosaurus/watchit)"
+fi
 
-qmllint qml/**/*.qml
+[[ -f ".env" ]] && source ./.env  # source .env for SSH_TARGET
+cSSH_TARGET=${SSH_TARGET:-"nemo@phone"}
 
-# Make sure the build is up-to-date.
-cargo build --target="${TARGET_ARCH}"
+# base path of files on the target device
+cSHADOW="/opt/sdk/harbour-whisperfish"
 
-# Also make sure translations are up-to-date
-./update-translations.sh
-for filepath in translations/*.ts;
-do
-	lrelease -idbased "${filepath}" -qm "${filepath%.*}.qm";
-done
+# Set this to non-null to disable building. Can be specified as environment
+# variable or as command line argument (NO_BUILD=1 live-patch.sh)
+# The executable is expected in build/harbour-whisperfish. Extract it from the
+# latest RPM.
+cNO_BUILDING="${NO_BUILD:+no-building}"
+[[ "$1" == NO_BUILD* ]] && cNO_BUILDING=no-building
 
-# Query the effective Cargo target directory
-TARGET_DIR="$(cargo metadata --format-version=1 | jq -r ".target_directory")"
+if [[ -z "$cNO_BUILDING" ]]; then
+    # query the effective Cargo target directory
+    cTARGET_DIR="$(cargo metadata --format-version=1 | jq -r ".target_directory")"
+    cTARGET_ARCH='armv7hl'
+    cLOCAL_EXE="${cTARGET_DIR}/${cTARGET_ARCH}/debug/harbour-whisperfish"
+else
+    cLOCAL_EXE=build/harbour-whisperfish  # extract from latest RPM
+fi
 
-# Copying system files to the target device requires SSH root privileges
-#
-# To enable this (assuming `sudo` is installed):
-#   1. Connect to the target as user `nemo` then:
-#     1. Run `sudo nano /etc/ssh/sshd_config`
-#     2. Add the line `PermitRootLogin yes` somewhere and exit
-#     3. Run `sudo systemctl restart sshd` to apply the changes
-#     4. Run `sudo passwd root` and set some temporary root password
-#   2. On your computer run `ssh-copy-id root@${SSH_TARGET}` to install your public key on the device
-#      (will require the temporary root password)
-#   3. You should now be able to use public-key based login for `root@${SSH_TARGET}`
-#   4. On the target revert the previous changes:
-#     1. Run `sudo nano /etc/ssh/sshd_config` and remove the added `PermitRootLogin` line
-#     2. Run `sudo systemctl restart sshd` to apply this change
-#     3. Run `sudo passwd -d root` to uninstall the root password
+function restart_app() {
+    local cEXEC="usr/bin/harbour-whisperfish"
+    local cENVIRONMENT=("RUST_BACKTRACE=full" "RUST_LOG=harbour_whisperfish=trace,actix=*,awc=*,actix-web=*,libsignal_service=trace,libsignal_service_actix=trace,debug")
+    # local cENVIRONMENT=("RUST_LOG=debug")
 
-# The list below mirrors the on in `Cargo.toml`'s `package.metadata.rpm.files` and ideally could be deduplicated
-echo
-echo " • Updating files on target"
-rsync -avzP  "${TARGET_DIR}/${TARGET_ARCH}/debug/harbour-whisperfish" "root@${SSH_TARGET}:/usr/bin/harbour-whisperfish"
-rsync -avzP  harbour-whisperfish.desktop                              "root@${SSH_TARGET}:/usr/share/applications/harbour-whisperfish.desktop"
-rsync -avzP  harbour-whisperfish-message.conf                         "root@${SSH_TARGET}:/usr/share/lipstick/notificationcategories/harbour-whisperfish-message.conf"
-rsync -avzP  icons/86x86/harbour-whisperfish.png                      "root@${SSH_TARGET}:/usr/share/icons/hicolor/86x86/apps/harbour-whisperfish.png"
-rsync -RavzP qml/ icons/ translations/ --exclude 'translations/*.ts'  "root@${SSH_TARGET}:/usr/share/harbour-whisperfish/"
+    echo && echo "••••• terminating currently running instance"
+    ssh "$cSSH_TARGET" killall --wait "$cSHADOW/$cEXEC" || true
+    # Use -tt to force-force-allocate a terminal even though it's in the
+    # background. This triggers Qt to print log output.
+    ( ssh -tt "$cSSH_TARGET" "${cENVIRONMENT[@]}" "$cSHADOW/$cEXEC" ) &
+}
 
-echo
-echo " • Terminating currently running application"
-ssh "nemo@${SSH_TARGET}" killall --wait harbour-whisperfish ||:
+function push() { # 1: source, 2: dest, 3: 'with-mkdir'?
+    local src="$1"; local dest="$cSHADOW/$2"; local make_dirs="${3:-no-mkdir}"
+    echo " - pushing '$src' to '$dest'"
 
-echo
-echo " • Starting harbour-whisperfish"
-# Use -t to force-allocate a terminal, it triggers Qt to log warnings.
-ssh -t "nemo@${SSH_TARGET}" "RUST_BACKTRACE=full RUST_LOG=harbour_whisperfish=trace,actix=*,awc=*,actix-web=*,libsignal_service=trace,libsignal_service_actix=trace,debug harbour-whisperfish"
+    if [[ "$make_dirs" == "with-mkdir" ]]; then
+        if [[ "$dest" == */ ]]; then
+            ssh "$cSSH_TARGET" mkdir -p "$dest" || { error 10 "failed to prepare dir '$dest'"; }
+        else
+            ssh "$cSSH_TARGET" mkdir -p "$(dirname "$dest")" || { error 10 "failed to prepare dir '$(dirname "$dest")'"; }
+        fi
+    fi
+
+    rsync -avzP --delete "$src" "$cSSH_TARGET:$dest" || { error 10 "failed to copy files"; }
+}
+
+function update_translations() {
+    echo && echo "••••• updating translations"
+    mkdir -p build/translations
+    lupdate-qt5 qml/ -noobsolete -ts translations/*.ts
+
+    for filepath in translations/*.ts; do
+        lrelease-qt5 -idbased "${filepath}" -qm "build/${filepath%.*}.qm";
+    done
+}
+
+function refresh_qml() { # 1: 'with-mkdir'?
+    local make_dirs="${1:-no-mkdir}"
+    echo && echo "••••• updating QML files on target"
+    # qmllint qml/**/*.qml
+    update_translations
+    push qml                                 "usr/share/harbour-whisperfish/"   "$make_dirs"
+    push build/translations                  "usr/share/harbour-whisperfish/" # "$make_dirs"
+}
+
+function refresh_files() { # 1: 'with-mkdir'?
+    local make_dirs="${1:-no-mkdir}"
+    # the list below mirrors the one in Cargo.toml's package.metadata.rpm.files and ideally could be deduplicated
+    echo && echo "••••• updating files on target"
+    push "$cLOCAL_EXE"                       "usr/bin/harbour-whisperfish" "$make_dirs"
+    push harbour-whisperfish.desktop         "usr/share/applications/harbour-whisperfish.desktop" "$make_dirs"
+    push harbour-whisperfish-message.conf    "usr/share/lipstick/notificationcategories/harbour-whisperfish-message.conf" "$make_dirs"
+    push icons/86x86/harbour-whisperfish.png "usr/share/icons/hicolor/86x86/apps/harbour-whisperfish.png" "$make_dirs"
+    push icons                               "usr/share/harbour-whisperfish/" # "$make_dirs"
+    refresh_qml "$make_dirs"
+}
+
+function main() {
+    if [[ -z "$cNO_BUILDING" ]]; then
+        # make sure the build is up-to-date
+        cargo build --target="${TARGET_ARCH}"
+    fi
+
+    refresh_files with-mkdir
+    restart_app
+
+    while event="$(watchit . -wsg "$cLOCAL_EXE" '*.qml' '*.qm' '*.ts' '*.js' '*.conf' '*.desktop' '*.png' '*.rs')"; do
+        printf "%s\n" "$event"
+        if [[ "$event" =~ \.rs$ ]] && [[ -z "$cNO_BUILDING" ]]; then
+            cargo build --target="${TARGET_ARCH}"
+            refresh_files
+        elif [[ "$event" =~ \.qml$ || "$event" =~ \.js$ ]]; then
+            refresh_qml
+        else
+            refresh_files
+        fi
+        restart_app
+    done
+}
+
+main
