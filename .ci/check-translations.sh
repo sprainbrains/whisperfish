4e914a0547c9cc43f41607a230b736a37bc25da0
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-02-03 16:08:18 +0100

    CI: comment on commits that trigger translation changes

---
+++ .ci/check-translations.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+TRANSLATION_WARNING="
+⚠️ This commit will trigger a change in the 🌐 translation 🌐 files. ⚠️
+
+Make sure to [trigger a Weblate commit](https://hosted.weblate.org/commit/whisperfish/whisperfish-application/) and to [merge the outstanding Weblate merge request](https://gitlab.com/rubdos/whisperfish/-/merge_requests/) before pulling in this merge request.
+
+Updating the translations will happen *outside* of any merge request, in order to avoid conflicts with Weblate.
+"
+
+lupdate qml/ -ts translations/*.ts
+if git diff --exit-code; then
+    echo "No translation update needed";
+else
+    curl --request POST \
+        --header "PRIVATE-TOKEN: $CI_JOB_TOKEN" \
+        --form "note=$TRANSLATION_WARNING" \
+        "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/commits/$CI_COMMIT_SHA/comments"
+fi
