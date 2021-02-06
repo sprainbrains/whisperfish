1210a892e2bdf9c31bc6a65013625c039e1b04ee
Gabriel Margiani <margianig@gmail.com>
2021-02-06 11:57:02 +0100

    Add sqlcipher code, compile it and link it statically

--- build.rs
+++ build.rs
@@ -287,13 +287,57 @@ fn main() {
     }
 
     let sailfish_libs: &[&str] = if cross_compile {
-        &["nemonotifications", "sailfishapp"]
+        &["nemonotifications", "sailfishapp", "qt5embedwidget"]
     } else {
         &[]
     };
     let libs = ["EGL"];
     for lib in libs.iter().chain(sailfish_libs.iter()) {
         println!("cargo:rustc-link-lib{}={}", macos_lib_search, lib);
+    }
+
+    if cross_compile {
+        if !Path::new("sqlcipher/sqlite3.c").is_file() {
+            // Download and prepare sqlcipher source
+            let stat = Command::new("sqlcipher/get-sqlcipher.sh")
+            .status()
+            .expect("Failed to download sqlcipher");
+            assert!(stat.success());
+        }
+
+        // Build static sqlcipher
+        cc::Build::new()
+        .include(format!("{}/usr/include/", mer_target_root))
+        .include(format!("{}/usr/include/openssl", mer_target_root))
+        .file("sqlcipher/sqlite3.c")
+        .warnings(false)
+        .flag("-DSQLITE_CORE")
+        .flag("-DSQLITE_DEFAULT_FOREIGN_KEYS=1")
+        .flag("-DSQLITE_ENABLE_API_ARMOR")
+        .flag("-DSQLITE_HAS_CODEC")
+        .flag("-DSQLITE_TEMP_STORE=2")
+        .flag("-DHAVE_ISNAN")
+        .flag("-DHAVE_LOCALTIME_R")
+        .flag("-DSQLITE_ENABLE_COLUMN_METADATA")
+        .flag("-DSQLITE_ENABLE_DBSTAT_VTAB")
+        .flag("-DSQLITE_ENABLE_FTS3")
+        .flag("-DSQLITE_ENABLE_FTS3_PARENTHESIS")
+        .flag("-DSQLITE_ENABLE_FTS5")
+        .flag("-DSQLITE_ENABLE_JSON1")
+        .flag("-DSQLITE_ENABLE_LOAD_EXTENSION=1")
+        .flag("-DSQLITE_ENABLE_MEMORY_MANAGEMENT")
+        .flag("-DSQLITE_ENABLE_RTREE")
+        .flag("-DSQLITE_ENABLE_STAT2")
+        .flag("-DSQLITE_ENABLE_STAT4")
+        .flag("-DSQLITE_SOUNDEX")
+        .flag("-DSQLITE_THREADSAFE=1")
+        .flag("-DSQLITE_USE_URI")
+        .flag("-DHAVE_USLEEP=1")
+        .compile("sqlcipher");
+
+        println!("cargo:lib_dir={}", env::var("OUT_DIR").unwrap());
+        println!("cargo:rustc-link-lib=static=sqlcipher");
+        println!("cargo:rerun-if-changed={}", "sqlcipher/sqlite3.c");
     }
 
     // vergen
