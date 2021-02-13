d278d4a61086fa20fe81d4c3d4f653c14e43b144
Ruben De Smet <weblate@rubdos.be>
2021-02-13 16:35:06 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (164 of 164 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/nl/

--- translations/harbour-whisperfish-nl.ts
+++ translations/harbour-whisperfish-nl.ts
@@ -61,7 +61,7 @@
         <source>Failed to setup data storage</source>
         <oldsource>Failed to setup datastore</oldsource>
         <extracomment>Failed to setup datastore error message</extracomment>
-        <translation type="unfinished">Instellen van gegevensopslag mislukt</translation>
+        <translation>Instellen van gegevensopslag mislukt</translation>
     </message>
     <message id="whisperfish-session-section-today">
         <location filename="../qml/pages/MainPage.qml" line="111"/>
@@ -514,7 +514,7 @@ Summary of all selected recipients, e.g. &quot;Bob, Jane, 75553243&quot;</extrac
         <source>Repeat the password</source>
         <oldsource>Repeated password</oldsource>
         <extracomment>repeated password input label</extracomment>
-        <translation type="unfinished">Herhaal het wachtwoord</translation>
+        <translation>Herhaal het wachtwoord</translation>
     </message>
     <message id="whisperfish-password-repeated-label-wrong">
         <location filename="../qml/pages/SetupPasswordPage.qml" line="103"/>
