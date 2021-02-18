35f791221eeaa443355192ddb5df59835b6b3b28
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-02-18 09:47:16 +0100

    Add GPLv3+ attribution header to Signal 2020 header

--- migrations/2021-02-14-180549_signal-2020/up.sql
+++ migrations/2021-02-14-180549_signal-2020/up.sql
@@ -1,3 +1,24 @@
+-- This is the database format that supports most Signal features in December 2020.
+-- Big part of the structure is taken from Signal Android:
+-- app/src/main/java/org/thoughtcrime/securesms/database/*.java
+
+-- Original copyright statement for those files:
+--  Copyright (C) 2011 Whisper Systems
+--  Copyright (C) 2013 - 2017 Open Whisper Systems
+--
+--  This program is free software: you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation, either version 3 of the License, or
+--  (at your option) any later version.
+--
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+--
+--  You should have received a copy of the GNU General Public License
+--  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ----
 -- 1. Rename X to X_old
 
