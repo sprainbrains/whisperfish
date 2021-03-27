993e429144558a385b4abb777311d966728524bc
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-03-27 14:47:40 +0100

    Debug information in ci notification

--- .ci/send-matrix-build-notification.sh
+++ .ci/send-matrix-build-notification.sh
@@ -9,12 +9,17 @@ VERSION="$CARGO_VERSION.b$CI_PIPELINE_IID.$GIT_REF"
 PACKAGE_ID=$(curl -s "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages?package_name=harbour-whisperfish" | jq '.[] | select(.version == "$CARGO_VERSION") | .id')
 PAGES=$(curl -si -XHEAD "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/$PACKAGE_ID/package_files" |grep x-total-pages | sed -e 's/x-total-pages: //')
 
+echo "Checking only page $PAGES"
+
 JQ_FORMAT_LIST="
 .[]
     | select(.file_name | test(\"$GIT_REF\"))
     | \"<li><a href=\\\"$CI_PROJECT_URL/-/package_files/\"+(.id|tostring)+\"/download\\\">\"+.file_name+\"</a></li>\"
 "
-DOWNLOAD_LIST=$(curl -s "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/$PACKAGE_ID/package_files?page=$PAGES" | jq -r "$JQ_FORMAT_LIST")
+DOWNLOAD_LIST=$(curl -s "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/$PACKAGE_ID/package_files?page=$PAGES")
+echo "Contents of page ("$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/$PACKAGE_ID/package_files?page=$PAGES") $PAGES"
+echo $DOWNLOAD_LIST
+DOWNLOAD_LIST=$(echo "$DOWNLOAD_LIST" | jq -r "$JQ_FORMAT_LIST")
 
 FORMATTED="🆕 New builds🧱 ($VERSION) are ready at https://gitlab.com/rubdos/whisperfish/-/packages 🥳 <ul>$DOWNLOAD_LIST</ul>"
 MSG="{\"msgtype\":\"m.text\", \"format\": \"org.matrix.custom.html\", \"body\":\"New builds are ready at https://gitlab.com/rubdos/whisperfish/-/packages\", \"formatted_body\": \"$FORMATTED\"}"
