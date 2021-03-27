50cf834f6ef13a1daf35768dddc364023001fbeb
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-03-27 12:59:53 +0100

    Send message to Matrix channel for new available builds

---
+++ .ci/send-matrix-build-notification.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+CARGO_VERSION="$(grep -m1 -e '^version\s=\s"' Cargo.toml | sed -e 's/.*"\(.*\)"/\1/')"
+GIT_REF="$(git rev-parse --short HEAD)"
+VERSION="$CARGO_VERSION.b$CI_PIPELINE_IID.$GIT_REF"
+
+PACKAGE_ID=$(curl -s "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages?package_name=harbour-whisperfish" | jq '.[] | select(.version == "$CARGO_VERSION") | .id')
+PAGES=$(curl -si -XHEAD "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/$PACKAGE_ID/package_files" |grep x-total-pages | sed -e 's/x-total-pages: //')
+
+JQ_FORMAT_LIST="
+.[]
+    | select(.file_name | test(\"$GIT_REF\"))
+    | \"<li><a href=\\\"$CI_PROJECT_URL/-/package_files/\"+(.id|tostring)+\"/download\\\">\"+.file_name+\"</a></li>\"
+"
+DOWNLOAD_LIST=$(curl -s "$CI_API_V4_URL/projects/$CI_PROJECT_ID/packages/$PACKAGE_ID?page=$PAGES" | jq -r $JQ_FORMAT_LIST)
+
+FORMATTED="🆕 New builds🧱 ($VERSION) are ready at https://gitlab.com/rubdos/whisperfish/-/packages 🥳 <ul>$DOWNLOAD_LIST</ul>"
+MSG="{\"msgtype\":\"m.text\", \"body\":\"New builds are ready at https://gitlab.com/rubdos/whisperfish/-/packages\", \"formatted_body\": \"$FORMATTED\"}"
+echo Sending $MSG
+
+curl -XPOST -d "$MSG" "$MATRIX_HOME_SERVER/_matrix/client/r0/rooms/$MATRIX_ROOM/send/m.room.message?access_token=$MATRIX_ACCESS_TOKEN"
