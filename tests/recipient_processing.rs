b991b7f099faa00b43782c8371464672873ee9b5
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-04-04 10:52:01 +0200

    Test e164 recipient upgrade

--- tests/recipient_processing.rs
+++ tests/recipient_processing.rs
@@ -13,7 +13,23 @@ fn storage_with_e164_recipient(
     storage: impl Future<Output = InMemoryDb>,
 ) -> impl Future<Output = InMemoryDb> {
     use futures::prelude::*;
-    storage.map(|(storage, _temp_dir)| (storage, _temp_dir))
+    storage.map(|(storage, _temp_dir)| {
+        storage.fetch_or_insert_recipient_by_e164(E164);
+
+        (storage, _temp_dir)
+    })
+}
+
+#[fixture]
+fn storage_with_uuid_recipient(
+    storage: impl Future<Output = InMemoryDb>,
+) -> impl Future<Output = InMemoryDb> {
+    use futures::prelude::*;
+    storage.map(|(storage, _temp_dir)| {
+        storage.fetch_or_insert_recipient_by_uuid(UUID);
+
+        (storage, _temp_dir)
+    })
 }
 
 mod merge_and_fetch {
@@ -40,5 +56,38 @@ mod merge_and_fetch {
             storage.merge_and_fetch_recipient(Some(E164), Some(UUID), TrustLevel::Uncertain);
         assert_eq!(recipient.e164.as_deref(), None);
         assert_eq!(recipient.uuid.as_deref(), Some(UUID));
+    }
+
+    #[rstest]
+    #[actix_rt::test]
+    async fn trusted_amend_e164(storage_with_e164_recipient: impl Future<Output = InMemoryDb>) {
+        let (storage, _temp_dir) = storage_with_e164_recipient.await;
+
+        let recipient =
+            storage.merge_and_fetch_recipient(Some(E164), Some(UUID), TrustLevel::Certain);
+        assert_eq!(recipient.e164.as_deref(), Some(E164));
+        assert_eq!(recipient.uuid.as_deref(), Some(UUID));
+
+        assert_eq!(storage.fetch_recipients().len(), 1);
+    }
+
+    #[rstest]
+    #[actix_rt::test]
+    async fn untrusted_amend_e164(storage_with_e164_recipient: impl Future<Output = InMemoryDb>) {
+        let (storage, _temp_dir) = storage_with_e164_recipient.await;
+
+        let recipient =
+            storage.merge_and_fetch_recipient(Some(E164), Some(UUID), TrustLevel::Uncertain);
+        assert_eq!(recipient.e164.as_deref(), None);
+        assert_eq!(recipient.uuid.as_deref(), Some(UUID));
+
+        // Now check that the e164 still exists separately.
+        let recipient_e164 = storage
+            .fetch_recipient(Some(E164), None)
+            .expect("e164 still in db");
+        assert_eq!(recipient_e164.e164.as_deref(), Some(E164));
+        assert_eq!(recipient_e164.uuid.as_deref(), None);
+
+        assert_eq!(storage.fetch_recipients().len(), 2);
     }
 }
