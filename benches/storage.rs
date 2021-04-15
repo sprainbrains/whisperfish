8a4508eaf527f9b67fe3129b1490fb60c22449b1
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-04-15 19:18:10 +0200

    Add benchmark for fetch_augmented_messages

---
+++ benches/storage.rs
@@ -0,0 +1,77 @@
+use harbour_whisperfish::store::{temp, NewMessage};
+use harbour_whisperfish::store::{Storage, StorageLocation};
+
+pub type InMemoryDb = (Storage, StorageLocation<tempdir::TempDir>);
+
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+
+pub fn storage() -> InMemoryDb {
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .build()
+        .unwrap();
+    rt.block_on(async {
+        let temp = temp();
+        (
+            Storage::new(&temp, None, 12345, "Some Password", [0; 52])
+                .await
+                .expect("Failed to initalize storage"),
+            temp,
+        )
+    })
+}
+
+fn fetch_augmented_messages(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fetch_augmented_messages");
+    group.significance_level(0.05).sample_size(20);
+    for elements in (9..18).map(|x| 1 << x) {
+        group.throughput(Throughput::Elements(elements));
+        for attachments in 0..3 {
+            // for receipts in (0..6) {
+            let (mut storage, _loc) = storage();
+            // Insert `elements` messages
+            let session = storage.fetch_or_insert_session_by_e164("+32474");
+            for _ in 0..elements {
+                let (msg, _) = storage.process_message(
+                    NewMessage {
+                        session_id: Some(session.id),
+                        source_e164: Some("+32474".into()),
+                        source_uuid: None,
+                        text: "Foo bar".into(),
+                        timestamp: chrono::Utc::now().naive_utc(),
+                        sent: false,
+                        received: false,
+                        is_read: false,
+                        flags: 0,
+                        attachment: None,
+                        mime_type: None,
+                        has_attachment: false,
+                        outgoing: false,
+                    },
+                    None,
+                );
+                for _attachment in 0..attachments {
+                    storage.register_attachment(msg.id, "", "");
+                }
+                // for _receipt in 0..receipts {
+                //     storage.register_attachment(msg.id, "", "");
+                // }
+            }
+            group.bench_with_input(
+                BenchmarkId::from_parameter(format!(
+                    "{} messages/{} attachments",
+                    elements, attachments
+                )),
+                &elements,
+                move |b, _| {
+                    // Now benchmark the retrieve function
+                    b.iter(|| black_box(storage.fetch_all_messages_augmented(session.id)))
+                },
+            );
+            // }
+        }
+    }
+    group.finish();
+}
+
+criterion_group!(benches, fetch_augmented_messages);
+criterion_main!(benches);
