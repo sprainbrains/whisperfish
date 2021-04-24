ff425aba3d59669306c89960a25b87dc0bc73a90
Matti Viljanen <matti.viljanen@kapsi.fi>
2021-04-24 13:30:08 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (215 of 215 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/fi/

--- translations/harbour-whisperfish-fi.ts
+++ translations/harbour-whisperfish-fi.ts
@@ -953,13 +953,13 @@ Settings page share contacts</extracomment>
         <location filename="../qml/pages/GroupProfilePage.qml" line="78"/>
         <source>Updated to the new group format</source>
         <extracomment>Indicator for updated groups</extracomment>
-        <translation type="unfinished">Päivitetty uuteen ryhmämuotoon</translation>
+        <translation>Päivitetty uuteen ryhmämuotoon</translation>
     </message>
     <message id="whisperfish-group-not-updated-to-groupv2">
         <location filename="../qml/pages/GroupProfilePage.qml" line="81"/>
         <source>Not updated to the new group format</source>
         <extracomment>Indicator for not yet updated groups</extracomment>
-        <translation type="unfinished">Ei päivitetty uuteen ryhmämuotoon</translation>
+        <translation>Ei päivitetty uuteen ryhmämuotoon</translation>
     </message>
     <message id="whisperfish-group-leave-menu">
         <location filename="../qml/pages/GroupProfilePage.qml" line="187"/>
