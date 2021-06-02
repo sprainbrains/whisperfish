ca6dc31c547f9603111caaa6d2897a7530a9662c
J. Lavoie <j.lavoie@net-c.ca>
2021-06-02 22:10:02 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (218 of 218 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/de/

--- translations/harbour-whisperfish-de.ts
+++ translations/harbour-whisperfish-de.ts
@@ -854,7 +854,7 @@ Settings page share contacts</extracomment>
         <location filename="../qml/pages/Settings.qml" line="379"/>
         <source>Compress database</source>
         <extracomment>Settings page &apos;Compress database&apos; button: execute &apos;VACUUM&apos; command on SQLite-database</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Datenbank komprimieren</translation>
     </message>
     <message id="whisperfish-settings-stats-section">
         <location filename="../qml/pages/Settings.qml" line="390"/>
