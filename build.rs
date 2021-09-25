a9bfc31fab78e5e9075b7f471d074bbd0086c65d
Ruben De Smet <ruben.de.smet@rubdos.be>
2021-09-25 19:45:09 +0200

    Sync up the qmake_query from qmetaobject

--- build.rs
+++ build.rs
@@ -19,17 +19,24 @@ use std::process::Command;
 
 use failure::*;
 
-fn qmake_query(var: &str) -> String {
+fn qmake_query(var: &str) -> Result<String, std::io::Error> {
     let qmake = std::env::var("QMAKE").unwrap_or_else(|_| "qmake".to_string());
-    String::from_utf8(
-        Command::new(qmake)
-            .env("QT_SELECT", "qt5")
-            .args(&["-query", var])
-            .output()
-            .expect("Failed to execute qmake. Make sure 'qmake' is in your path")
-            .stdout,
-    )
-    .expect("UTF-8 conversion failed")
+    let output = Command::new(qmake).args(&["-query", var]).output()?;
+    if !output.status.success() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!(
+                "qmake returned with error:\n{}\n{}",
+                std::str::from_utf8(&output.stderr).unwrap_or_default(),
+                std::str::from_utf8(&output.stdout).unwrap_or_default()
+            ),
+        ));
+    }
+
+    Ok(std::str::from_utf8(&output.stdout)
+        .expect("UTF-8 conversion failed")
+        .trim()
+        .to_string())
 }
 
 #[cfg(feature = "bundled-sqlcipher")]
@@ -139,7 +146,7 @@ fn main() {
         }
     }
 
-    let qt_include_path = qmake_query("QT_INSTALL_HEADERS");
+    let qt_include_path = qmake_query("QT_INSTALL_HEADERS").expect("QMAKE");
     let qt_include_path = qt_include_path.trim();
 
     let mut cfg = cpp_build::Config::new();
