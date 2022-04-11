97d898d5a22debf59f4691b5efce279b1d405153
Ruben De Smet <ruben.de.smet@rubdos.be>
2022-04-11 16:09:01 +0200

    CI: Fix send-matrix-build-notification

--- .ci/send-matrix-build-notification.sh
+++ .ci/send-matrix-build-notification.sh
@@ -16,7 +16,7 @@ echo "Checking only page $PAGES"
 
 JQ_FORMAT_LIST="
 [.[]
-    | select((.file_name | test(\"$GIT_REF\")) and (not (.file_name | test(\"-debug\"))))
+    | select((.file_name | test(\"$GIT_REF\")) and ((.file_name | test(\"-debug\")) | not))
     | \"<li><a href=\\\\\\\"$CI_PROJECT_URL/-/package_files/\"+(.id|tostring)+\"/download\\\\\\\">\"+.file_name+\"</a></li>\" ]
     | join(\" \")
 "
