65e2557d14c3033c95944f9c39cd75905b56fe69
J. Lavoie <j.lavoie@net-c.ca>
2022-06-25 07:43:38 +0000

    Translated using Weblate (German)
    Currently translated at 99.1% (240 of 242 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/de/

--- translations/harbour-whisperfish-de.ts
+++ translations/harbour-whisperfish-de.ts
@@ -1548,9 +1548,9 @@ Settings page share contacts</extracomment>
         <location filename="../qml/cover/CoverPage.qml" line="33"/>
         <source>Unread&lt;br/&gt;message(s)</source>
         <extracomment>Unread messages count cover label. Code requires exact line break tag &quot;&lt;br/&gt;&quot;.</extracomment>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Ungelesene&lt;br/&gt;Nachricht</numerusform>
+            <numerusform>Ungelesene&lt;br/&gt;Nachrichten</numerusform>
         </translation>
     </message>
 </context>
