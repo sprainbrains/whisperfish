2feab737fa01fb39003042f126313840915ab252
J. Lavoie <j.lavoie@net-c.ca>
2022-07-12 11:40:43 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (244 of 244 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/fr/

--- translations/harbour-whisperfish-fr.ts
+++ translations/harbour-whisperfish-fr.ts
@@ -1051,7 +1051,7 @@ Settings page share contacts</extracomment>
         <source>Show debugging information and controls in the user interface.</source>
         <oldsource>Show debugging information in the user interface.</oldsource>
         <extracomment>Settings page: debug info toggle extended description</extracomment>
-        <translation type="unfinished">Afficher les informations de débogage dans l&apos;interface utilisateur.</translation>
+        <translation>Afficher les informations de débogage dans l&apos;interface utilisateur.</translation>
     </message>
     <message id="whisperfish-settings-compress-db">
         <location filename="../qml/pages/Settings.qml" line="396"/>
@@ -1148,13 +1148,13 @@ Settings page share contacts</extracomment>
         <location filename="../qml/pages/VerifyIdentity.qml" line="19"/>
         <source>Reset identity key</source>
         <extracomment>Reset identity key menu item</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Réinitialissr la clé d&apos;identité</translation>
     </message>
     <message id="whisperfish-reset-identity-message">
         <location filename="../qml/pages/VerifyIdentity.qml" line="24"/>
         <source>Identity key reset</source>
         <extracomment>Reset identity key remorse message (past tense)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Clé d&apos;identité réinitialisée</translation>
     </message>
     <message id="whisperfish-reset-session-menu">
         <location filename="../qml/pages/VerifyIdentity.qml" line="34"/>
