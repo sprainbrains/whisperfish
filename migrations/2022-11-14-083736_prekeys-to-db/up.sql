86b616a0405487a355c27622c0443ba7c3e036e8
Ruben De Smet <ruben.de.smet@rubdos.be>
2022-11-14 16:22:03 +0100

    Create prekey and signed prekey tables

---
+++ migrations/2022-11-14-083736_prekeys-to-db/up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE signed_prekeys (
+    id INTEGER PRIMARY KEY NOT NULL,
+    record BLOB NOT NULL
+);
+
+CREATE TABLE prekeys (
+    id INTEGER PRIMARY KEY NOT NULL,
+    record BLOB NOT NULL
+);
--- whisperfish/src/schema.rs
+++ whisperfish/src/schema.rs
@@ -107,6 +107,13 @@ table! {
 }
 
 table! {
+    prekeys (id) {
+        id -> Integer,
+        record -> Binary,
+    }
+}
+
+table! {
     reactions (reaction_id) {
         reaction_id -> Integer,
         message_id -> Integer,
@@ -188,6 +195,13 @@ table! {
 }
 
 table! {
+    signed_prekeys (id) {
+        id -> Integer,
+        record -> Binary,
+    }
+}
+
+table! {
     stickers (pack_id, sticker_id) {
         pack_id -> Nullable<Text>,
         sticker_id -> Integer,
@@ -228,11 +242,13 @@ allow_tables_to_appear_in_same_query!(
     group_v2s,
     identity_records,
     messages,
+    prekeys,
     reactions,
     receipts,
     recipients,
     sender_key_records,
     session_records,
     sessions,
+    signed_prekeys,
     stickers,
 );
