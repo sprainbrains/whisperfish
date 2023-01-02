791efd979fd50d05465df1c797e2de8656e6d267
Ruben De Smet <ruben.de.smet@rubdos.be>
2023-01-02 17:04:45 +0100

    Create a recipientlistmodel

--- whisperfish/src/model/recipient.rs
+++ whisperfish/src/model/recipient.rs
@@ -5,6 +5,7 @@ use crate::store::observer::EventObserving;
 use crate::store::{orm, Storage};
 use qmeta_async::with_executor;
 use qmetaobject::prelude::*;
+use std::collections::HashMap;
 
 /// QML-constructable object that interacts with a single recipient.
 #[derive(Default)]
@@ -48,5 +49,52 @@ impl RecipientImpl {
         if let Some(id) = self.recipient_id {
             self.recipient = storage.fetch_recipient_by_id(id);
         }
+    }
+}
+
+#[derive(QObject, Default)]
+pub struct RecipientListModel {
+    base: qt_base_class!(trait QAbstractListModel),
+    content: Vec<orm::Recipient>,
+}
+
+impl RecipientListModel {}
+
+define_model_roles! {
+    enum RecipientRoles for orm::Recipient {
+        Id(id): "id",
+        Uuid(uuid via qstring_from_option): "uuid",
+        // These two are aliases
+        E164(e164 via qstring_from_option): "e164",
+        PhoneNumber(e164 via qstring_from_option): "phoneNumber",
+        Username(username via qstring_from_option): "username",
+        Email(email via qstring_from_option): "email",
+
+        Blocked(blocked): "blocked",
+
+        JoinedName(profile_joined_name via qstring_from_option): "name",
+        FamilyName(profile_family_name via qstring_from_option): "familyName",
+        GivenName(profile_given_name via qstring_from_option): "givenName",
+
+        About(about via qstring_from_option): "about",
+        Emoji(about_emoji via qstring_from_option): "emoji",
+
+        UnidentifiedAccessModel(unidentified_access_mode): "unidentifiedAccessMode",
+        ProfileSharing(profile_sharing): "profileSharing",
+    }
+}
+
+impl QAbstractListModel for RecipientListModel {
+    fn row_count(&self) -> i32 {
+        self.content.len() as _
+    }
+
+    fn data(&self, index: QModelIndex, role: i32) -> QVariant {
+        let role = RecipientRoles::from(role);
+        role.get(&self.content[index.row() as usize])
+    }
+
+    fn role_names(&self) -> HashMap<i32, QByteArray> {
+        RecipientRoles::role_names()
     }
 }
