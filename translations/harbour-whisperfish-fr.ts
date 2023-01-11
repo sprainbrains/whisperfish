43da4e345c05fd9b1ae3607c8f4fa218b80c3bc6
J. Lavoie <j.lavoie@net-c.ca>
2023-01-11 00:33:55 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (260 of 260 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/fr/

--- translations/harbour-whisperfish-fr.ts
+++ translations/harbour-whisperfish-fr.ts
@@ -1386,7 +1386,7 @@ Settings page share contacts</extracomment>
         <location filename="../qml/components/QuotedMessagePreview.qml" line="111"/>
         <source>Unknown sender</source>
         <extracomment>Text shown on quotes when the sender of a quote is unknown</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Expéditeur inconnu</translation>
     </message>
     <message id="whisperfish-quoted-message-preview-attachment">
         <location filename="../qml/components/QuotedMessagePreview.qml" line="130"/>
