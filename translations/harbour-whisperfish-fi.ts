08348642256cf3ba124d270701a3a7668c52df9f
Matti Viljanen <matti.viljanen@kapsi.fi>
2023-03-21 20:07:45 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (262 of 262 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/fi/

--- translations/harbour-whisperfish-fi.ts
+++ translations/harbour-whisperfish-fi.ts
@@ -953,25 +953,25 @@ Settings page share contacts</extracomment>
         <location filename="../qml/pages/Settings.qml" line="327"/>
         <source>Unlock Password</source>
         <extracomment>Settings page autostart password field</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Avaussalasana</translation>
     </message>
     <message id="whisperfish-settings-save-password-button">
         <location filename="../qml/pages/Settings.qml" line="338"/>
         <source>Save password</source>
         <extracomment>Settings page save autologin password button</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tallenna salasana</translation>
     </message>
     <message id="whisperfish-settings-clear-password-button">
         <location filename="../qml/pages/Settings.qml" line="341"/>
         <source>clear password</source>
         <extracomment>Settings page clear autologin password button</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Tyhjennä salasana</translation>
     </message>
     <message id="whisperfish-settings-auto-unlock-password-info">
         <location filename="../qml/pages/Settings.qml" line="353"/>
         <source>You can enter your password to make Whisperfish unlock the database automatically at startup. Please note that the password is stored in plain text, and as such usage of this feature is not recommened.</source>
         <extracomment>Settings page info about setting auto unlock password</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Voit syöttää salasanasi ylläolevaan kenttään, jolloin Whisperfish avaa salauksen automaattisesti käynnistyksen yhteydessä. Huomioithan että salasanasi tallennetaan asetuksiin selkokielisenä, joten tämän ominaisuuden käyttö ei ole suositeltavaa.</translation>
     </message>
     <message id="whisperfish-settings-enable-background-mode-description">
         <location filename="../qml/pages/Settings.qml" line="375"/>
