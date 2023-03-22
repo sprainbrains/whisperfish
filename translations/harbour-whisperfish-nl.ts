3f42196fe48c8edc9bfe14833ca6b48682d507f5
Ruben De Smet <weblate@rubdos.be>
2023-03-22 20:43:16 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (262 of 262 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/nl/

--- translations/harbour-whisperfish-nl.ts
+++ translations/harbour-whisperfish-nl.ts
@@ -950,25 +950,25 @@ Settings page share contacts</extracomment>
         <location filename="../qml/pages/Settings.qml" line="327"/>
         <source>Unlock Password</source>
         <extracomment>Settings page autostart password field</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Ontgrendelingswachtwoord</translation>
     </message>
     <message id="whisperfish-settings-save-password-button">
         <location filename="../qml/pages/Settings.qml" line="338"/>
         <source>Save password</source>
         <extracomment>Settings page save autologin password button</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Sla wachtwoord op</translation>
     </message>
     <message id="whisperfish-settings-clear-password-button">
         <location filename="../qml/pages/Settings.qml" line="341"/>
         <source>clear password</source>
         <extracomment>Settings page clear autologin password button</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Vergeet wachtwoord</translation>
     </message>
     <message id="whisperfish-settings-auto-unlock-password-info">
         <location filename="../qml/pages/Settings.qml" line="353"/>
         <source>You can enter your password to make Whisperfish unlock the database automatically at startup. Please note that the password is stored in plain text, and as such usage of this feature is not recommened.</source>
         <extracomment>Settings page info about setting auto unlock password</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Je kan je wachtwoord opslaan om bij het starten van Whisperfish automatisch de database te ontgrendelen. Wees je ervan bewust dat het wachtwoord wordt opgeslagen in klare tekst en dat het gebruik van deze optie niet wordt aanbevolen.</translation>
     </message>
     <message id="whisperfish-settings-enable-background-mode-description">
         <location filename="../qml/pages/Settings.qml" line="375"/>
