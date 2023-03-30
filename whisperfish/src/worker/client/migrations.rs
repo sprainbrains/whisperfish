f9706d66830a9500e9ae60625c038eb1f69e5686
Ruben De Smet <ruben.de.smet@rubdos.be>
2023-03-30 10:27:41 +0200

    Remove E164-to-UUID migration
    Relates to #532

--- whisperfish/src/worker/client/migrations.rs
+++ whisperfish/src/worker/client/migrations.rs
@@ -2,9 +2,6 @@
 // XXX maybe the session-to-db migration should move into the store module.
 pub mod session_to_db;
 
-/// Migration to move files in `storage/sessions` and `storage/identity` to their
-/// UUID-based counterparts.
-mod e164_to_uuid;
 /// Migrations related to groupv2
 mod groupv2;
 /// Migration to remove R@ reactions and dump them in the correct table.
@@ -14,7 +11,6 @@ mod parse_reactions;
 /// Installs before Whisperfish 0.6 do not have their own UUID present in settings.
 mod whoami;
 
-use self::e164_to_uuid::*;
 use self::groupv2::*;
 use self::parse_reactions::*;
 use self::session_to_db::*;
@@ -112,7 +108,6 @@ impl MigrationCondVar {
 
     notify_method_for_var!(notify_whoami -> whoami);
     notify_method_for_var!(notify_protocol_store_in_db -> protocol_store_in_db);
-    notify_method_for_var!(notify_e164_to_uuid -> sessions_have_uuid);
     notify_method_for_var!(notify_groupv2_expected_ids -> gv2_expected_ids);
     notify_method_for_var!(notify_self_profile_ready -> self_profile_ready);
     notify_method_for_var!(notify_reactions_ready -> reactions_ready);
@@ -122,7 +117,6 @@ impl ClientActor {
     pub(super) fn queue_migrations(ctx: &mut <Self as Actor>::Context) {
         ctx.notify(WhoAmI);
         ctx.notify(MoveSessionsToDatabase);
-        ctx.notify(E164ToUuid);
         ctx.notify(ComputeGroupV2ExpectedIds);
         ctx.notify(RefreshOwnProfile { force: false });
         ctx.notify(ParseOldReaction);
