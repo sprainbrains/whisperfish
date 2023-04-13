3342dbba7ab53b6151c321b3bc46904b950c0433
Ruben De Smet <weblate@rubdos.be>
2023-04-13 12:08:18 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (277 of 277 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/nl/

--- translations/harbour-whisperfish-nl.ts
+++ translations/harbour-whisperfish-nl.ts
@@ -489,7 +489,7 @@
         <location filename="../qml/pages/ProfilePage.qml" line="189"/>
         <source>The recipient is not currently registered to Signal, so sending and receiving messages is not possible.</source>
         <extracomment>Profile page, user is not registered warning</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>De correspondent is momenteel niet geregistreerd bij Signal, dus is het verzenden en ontvangen van berichten onmogelijk.</translation>
     </message>
     <message id="whisperfish-profile-joined-name">
         <location filename="../qml/pages/ProfilePage.qml" line="202"/>
@@ -1478,7 +1478,7 @@ Settings page share contacts</extracomment>
         <location filename="../qml/components/ChatTextInput.qml" line="209"/>
         <source>The recipient is not registered</source>
         <extracomment>Chat text input placeholder for deleted/unregistered recipient</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Deze correspondent is niet geregistreerd</translation>
     </message>
     <message id="whisperfish-chat-input-placeholder-personal">
         <location filename="../qml/components/ChatTextInput.qml" line="214"/>
