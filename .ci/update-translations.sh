44a711f8bd8781f1c01d051bc40c2b4da9928a4b
Ruben De Smet <ruben.de.smet@rubdos.be>
2023-05-26 07:50:27 +0200

    Only auto-push translation updates in actual CI

--- .ci/update-translations.sh
+++ .ci/update-translations.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
 lupdate qml/ -ts translations/*.ts
-if git diff --exit-code; then
-    echo "No translation update needed";
-else
-    echo "Committing translation update";
-    git config --global user.email "whisperfish@rubdos.be"
-    git config --global user.name "Gitlab CI translation file sync"
-    git commit translations/ -m "Translation file synchronisation";
-    git remote add origin-ssh git@gitlab.com:whisperfish/whisperfish.git
-    git push origin-ssh HEAD:master
+
+if [ "$CI" = "true" ]; then
+    if git diff --exit-code; then
+        echo "No translation update needed";
+    else
+        echo "Committing translation update";
+        git config --global user.email "whisperfish@rubdos.be"
+        git config --global user.name "Gitlab CI translation file sync"
+        git commit translations/ -m "Translation file synchronisation";
+        git remote add origin-ssh git@gitlab.com:whisperfish/whisperfish.git
+        git push origin-ssh HEAD:master
+    fi
 fi
