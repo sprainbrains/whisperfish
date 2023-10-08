05811ea35a73982cf215af873542713cbcff06f2
bittin1ddc447d824349b2 <bittin@reimu.nl>
2023-10-08 07:33:56 +0000

    Translated using Weblate (Swedish)
    Currently translated at 99.7% (333 of 334 strings)
    
    Translation: Whisperfish/Whisperfish application
    Translate-URL: https://hosted.weblate.org/projects/whisperfish/whisperfish-application/sv/

--- translations/harbour-whisperfish-sv.ts
+++ translations/harbour-whisperfish-sv.ts
@@ -1498,7 +1498,7 @@ Settings page share contacts</extracomment>
         <location filename="../qml/pages/Settings.qml" line="272"/>
         <source>Show phone number</source>
         <extracomment>Settings page, show recipient phone number in conversation view</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Visa telefonnummer</translation>
     </message>
     <message id="whisperfish-settings-show-phone-number-description">
         <location filename="../qml/pages/Settings.qml" line="275"/>
